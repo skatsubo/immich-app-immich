28b6ffb3ef6f8092cdcb0d41ca2d40e01b0bd971
mertalev <101130780+mertalev@users.noreply.github.com>
2026-03-06 04:18:39 -0500

    update api

--- mobile/openapi/lib/model/audio_codec.dart
+++ mobile/openapi/lib/model/audio_codec.dart
@@ -25,6 +25,7 @@ class AudioCodec {
 
   static const mp3 = AudioCodec._(r'mp3');
   static const aac = AudioCodec._(r'aac');
+  static const libopus = AudioCodec._(r'libopus');
   static const opus = AudioCodec._(r'opus');
   static const pcmS16le = AudioCodec._(r'pcm_s16le');
 
@@ -32,6 +33,7 @@ class AudioCodec {
   static const values = <AudioCodec>[
     mp3,
     aac,
+    libopus,
     opus,
     pcmS16le,
   ];
@@ -74,6 +76,7 @@ class AudioCodecTypeTransformer {
       switch (data) {
         case r'mp3': return AudioCodec.mp3;
         case r'aac': return AudioCodec.aac;
+        case r'libopus': return AudioCodec.libopus;
         case r'opus': return AudioCodec.opus;
         case r'pcm_s16le': return AudioCodec.pcmS16le;
         default:
