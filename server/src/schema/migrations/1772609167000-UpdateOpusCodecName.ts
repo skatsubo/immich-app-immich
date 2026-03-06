693f8c002a0cfe135cc9c3c89be45e63f795377d
Sergey Katsubo <skatsubo@gmail.com>
2026-03-06 11:29:29 +0300

    Add db migration for ffmpeg.targetAudioCodec opus

--- server/src/schema/migrations/1772609167000-UpdateOpusCodecName.ts
+++ server/src/schema/migrations/1772609167000-UpdateOpusCodecName.ts
@@ -19,6 +19,17 @@ export async function up(db: Kysely<any>): Promise<void> {
     WHERE key = 'system-config'
     AND value->'ffmpeg'->'acceptedAudioCodecs' ? 'libopus';
   `.execute(db);
+
+  await sql`
+    UPDATE system_metadata
+    SET value = jsonb_set(
+      value,
+      '{ffmpeg,targetAudioCodec}',
+      '"opus"'::jsonb
+    )
+    WHERE key = 'system-config'
+    AND value->'ffmpeg'->>'targetAudioCodec' = 'libopus';
+  `.execute(db);
 }
 
 export async function down(db: Kysely<any>): Promise<void> {
@@ -39,5 +50,16 @@ export async function down(db: Kysely<any>): Promise<void> {
     )
     WHERE key = 'system-config'
     AND value->'ffmpeg'->'acceptedAudioCodecs' ? 'opus';
+  `.execute(db);
+
+  await sql`
+    UPDATE system_metadata
+    SET value = jsonb_set(
+      value,
+      '{ffmpeg,targetAudioCodec}',
+      '"libopus"'::jsonb
+    )
+    WHERE key = 'system-config'
+    AND value->'ffmpeg'->>'targetAudioCodec' = 'opus';
   `.execute(db);
 }
