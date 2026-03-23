3bdfc3daa25483ee9402806bc076305616499bf4
Sergey Katsubo <skatsubo@gmail.com>
2026-03-23 14:11:26 +0300

    Add Folder View

--- server/src/repositories/view-repository.ts
+++ server/src/repositories/view-repository.ts
@@ -8,13 +8,17 @@ import { asUuid, withExif } from 'src/utils/database';
 export class ViewRepository {
   constructor(@InjectKysely() private db: Kysely<DB>) {}
 
+  private static EXP_GROUP_READ = true;
+  // private static EXP_GROUP_READ = false;
+
   @GenerateSql({ params: [DummyValue.UUID] })
   async getUniqueOriginalPaths(userId: string) {
     const results = await this.db
       .selectFrom('asset')
       .select((eb) => eb.fn<string>('substring', ['asset.originalPath', eb.val('^(.*/)[^/]*$')]).as('directoryPath'))
       .distinct()
-      .where('ownerId', '=', asUuid(userId))
+      // .where('ownerId', '=', asUuid(userId))
+      .$if(!ViewRepository.EXP_GROUP_READ, (qb) => qb.where('ownerId', '=', asUuid(userId)))
       .where('visibility', '=', AssetVisibility.Timeline)
       .where('deletedAt', 'is', null)
       .where('fileCreatedAt', 'is not', null)
@@ -30,22 +34,25 @@ export class ViewRepository {
   async getAssetsByOriginalPath(userId: string, partialPath: string) {
     const normalizedPath = partialPath.replaceAll(/\/$/g, '');
 
-    return this.db
-      .selectFrom('asset')
-      .selectAll('asset')
-      .$call(withExif)
-      .where('ownerId', '=', asUuid(userId))
-      .where('visibility', '=', AssetVisibility.Timeline)
-      .where('deletedAt', 'is', null)
-      .where('fileCreatedAt', 'is not', null)
-      .where('fileModifiedAt', 'is not', null)
-      .where('localDateTime', 'is not', null)
-      .where('originalPath', 'like', `%${normalizedPath}/%`)
-      .where('originalPath', 'not like', `%${normalizedPath}/%/%`)
-      .orderBy(
-        (eb) => eb.fn('regexp_replace', ['asset.originalPath', eb.val('.*/(.+)'), eb.val(String.raw`\1`)]),
-        'asc',
-      )
-      .execute();
+    return (
+      this.db
+        .selectFrom('asset')
+        .selectAll('asset')
+        .$call(withExif)
+        // .where('ownerId', '=', asUuid(userId))
+        .$if(!ViewRepository.EXP_GROUP_READ, (qb) => qb.where('ownerId', '=', asUuid(userId)))
+        .where('visibility', '=', AssetVisibility.Timeline)
+        .where('deletedAt', 'is', null)
+        .where('fileCreatedAt', 'is not', null)
+        .where('fileModifiedAt', 'is not', null)
+        .where('localDateTime', 'is not', null)
+        .where('originalPath', 'like', `%${normalizedPath}/%`)
+        .where('originalPath', 'not like', `%${normalizedPath}/%/%`)
+        .orderBy(
+          (eb) => eb.fn('regexp_replace', ['asset.originalPath', eb.val('.*/(.+)'), eb.val(String.raw`\1`)]),
+          'asc',
+        )
+        .execute()
+    );
   }
 }
